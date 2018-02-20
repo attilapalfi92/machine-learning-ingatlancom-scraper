db45a46ad44218fc7066cdc124a006d3ea76a6a4
attila.palfi <attila.palfi@evosoft.com>
2018-02-20 15:34:24 +0100

    flat is created beautifully

--- src/main/java/com/attilapalfi/machinelearning/housingprices/Location.kt
+++
@@ -1,7 +0,0 @@
-package com.attilapalfi.machinelearning.housingprices
-
-data class Location (val settlement: String = "",
-                     val locationSub1: String = "",
-                     val locationSub2: String = "",
-                     val locationSub3: String = "",
-                     val locationSub4: String = "")
--- src/main/java/com/attilapalfi/machinelearning/housingprices/Main.kt
+++ src/main/java/com/attilapalfi/machinelearning/housingprices/Main.kt
@@ -4,40 +4,66 @@ import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import org.jsoup.nodes.Element
 
-// https://ingatlan.com/xvi-ker/elado+haz/ikerhaz/budapest+16+kerulet+vagas+utca/25367897
-// /vii-ker/elado+lakas/tegla-epitesu-lakas/budapest+7+kerulet/25534997
 
 fun main(args: Array<String>) {
     val propertyPage = getPropertyPage(1)
     propertyPage.map { it.select("a[title=\"Részletek\"]").attr("href") }
             .parallelStream()
-            .forEach{ getProperty(it) }
+            .map{ getFlat(it) }
+            .forEach { storeFlat(it) }
 }
-
-private fun getProperty(href: String) {
-    val flatPage = Jsoup.connect("https://ingatlan.com$href").get()
-    val size = flatPage.select("div.parameter-area-size").select("span.parameter-value").text()
-    val rooms = flatPage.select("div.parameter-room").select("span.parameter-value").text()
-    val price = flatPage.select("div.parameter-price").select("span.parameter-value").text()
-    val subType = flatPage.select("div.listing-subtype").text()
-    val location = getLocation(flatPage)
-    Flat(
-            price = price,
-            subType = subType,
-            size = size,
-            rooms = rooms,
-            buildingMaterial = subType
-    )
-}
-
-fun getLocation(flatPage: Document): Location {
-    val locationDiv = flatPage.select("#map-nav-links")
-    return Location()
-}
-
 
 private fun getPropertyPage(page: Int): List<Element> {
     val doc = Jsoup.connect("https://ingatlan.com/lista/elado+lakas?page=$page").get()
     val houseList = doc.select("div.resultspage__listings")
     return houseList[0].children().filter { e -> e.hasAttr("data-id") }
+}
+
+private fun getFlat(href: String) : Flat {
+    val flatPage: Document = Jsoup.connect("https://ingatlan.com$href").get()
+    val size = flatPage.select("div.parameter-area-size").select("span.parameter-value").text().trim()
+    val rooms = flatPage.select("div.parameter-room").select("span.parameter-value").text().trim()
+    val price = flatPage.select("div.parameter-price").select("span.parameter-value").text().trim()
+    val subType = flatPage.select("div.listing-subtype").text().trim()
+    val settlementString = flatPage.select("h1.js-listing-title").text().trim()
+    val settlement = settlementString.split(",")[0].trim()
+    val settlementSub = settlementString.split(",")[1].trim()
+
+    val params = flatPage.select("div.paramterers")
+    val elements = params[0].allElements.filter { it.`is`("td") }
+
+    return Flat(
+            price = price,
+            subType = subType,
+            size = size,
+            rooms = rooms,
+            buildingMaterial = subType,
+            settlement = settlement,
+            settlementSub = settlementSub,
+            condition = getParameter(elements, "Ingatlan állapota"),
+            buildingLevels = getParameter(elements, "Épület szintjei"),
+            floor = getParameter(elements, "Emelet"),
+            buildYear = getParameter(elements, "Építés éve"),
+            barrierFree = getParameter(elements, "Akadálymentesített"),
+            comfort = getParameter(elements, "Komfort"),
+            gardenConnected = getParameter(elements, "Kertkapcsolatos"),
+            toilet = getParameter(elements, "Fürdő és WC"),
+            energyCert = getParameter(elements, "Energiatanúsítvány"),
+            attic = getParameter(elements, "Tetőtér"),
+            heating = getParameter(elements, "Fűtés"),
+            parking = getParameter(elements, "Parkolás"),
+            ac = getParameter(elements, "Légkondicionáló")
+    )
+}
+
+private fun getParameter(elements: List<Element>, paramName: String): String {
+    val index = elements.indexOfFirst { it.text().contains(paramName) }
+    if (index == -1) {
+        return "nincs megadva"
+    }
+    return elements[index + 1].text()
+}
+
+fun storeFlat(it: Flat) {
+
 }
